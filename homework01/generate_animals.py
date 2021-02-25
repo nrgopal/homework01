177eb38883119f2f8b6b679254c07981eaebb754
Neel Gopal <ngopal@utexas.edu>
2021-02-25 02:51:20 -0600

    started tracking homework02 scripts and Dockerfile

--- homework01/generate_animals.py
+++ homework01/generate_animals.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import json
 import petname
 import random
+import sys
 
 def random_head():
     return(random.choice(['snake', 'bull', 'lion', 'raven', 'bunny']))
@@ -9,7 +12,7 @@ def random_body():
     return(random.choice(petname.names)) + '-' + (random.choice(petname.names))
 
 def random_arms():
-    num_of_arms = random.randrange(2,11)
+    num_of_arms = random.randrange(2, 11 ,2)
     random_arms.arms = num_of_arms
     return(num_of_arms)
 
@@ -27,7 +30,7 @@ data = {}
 data['animals'] = []
 
 for i in range(20):
-    data['animals'].append({'head:': random_head(), 'body:': random_body(), 'arms:': random_arms(), 'legs:': random_legs(), 'tail:': num_tails(random_arms.arms, random_legs.legs)})
+    data['animals'].append({'head': random_head(), 'body': random_body(), 'arms': random_arms(), 'legs': random_legs(),'tails': num_tails(random_arms.arms, random_legs.legs)})
 
-with open ('animals.json', 'w') as out:
-    json.dump(data, out, indent=2)
+with open(sys.argv[1], 'w') as f:          
+    json.dump(data, f, indent=2)  
