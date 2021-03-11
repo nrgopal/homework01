ec4fbdf827f3ec3e99ffd67e162d452c73a97d04
nrgopal <78436756+nrgopal@users.noreply.github.com>
2021-03-11 10:39:31 -0600

    track app.py

--- homework03/app.py
+++ homework03/app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, request                                                                                                                                                                                   
+from flask import Flask, request
+
 app = Flask(__name__)
 
 def getdata():
@@ -15,14 +16,15 @@ def get_animals():
 
 @app.route('/animals/head', methods=['GET'])
 def get_bunny_heads():
-    animals = getdata()                                                                                                                                                                                                head = request.args.get('head')
-    return json.dumps([x for x in animals if x['head'] == head]) 
+    animals = getdata()
+    head = request.args.get('head')
+    return json.dumps([x for x in animals if x['head'] == head])
 
 @app.route('/animals/legs', methods=['GET'])
 def get_six_legs():
     animals = getdata()
-    legs = int(request.args.get('legs')
-    return json.dumps([x for x in animals if x['legs'] == legs)
+    legs = int(request.args.get('legs'))
+    return json.dumps([x for x in animals if x['legs'] == legs])
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0') 
+    app.run(debug = True, host = '0.0.0.0')
