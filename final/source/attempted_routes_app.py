7f35c511d199b179426e8b73e2593f0db0c038f0
nrgopal <78436756+nrgopal@users.noreply.github.com>
2021-05-08 07:06:08 -0500

    Update and rename attempted_routes.py to attempted_routes_app.py

--- final/source/attempted_routes_app.py
+++ final/source/attempted_routes_app.py
@@ -1,3 +1,5 @@
+### These were attempted routes to edit the local dataset post submitting CRUD jobs.
+
 import json
 from flask import Flask, request
 import jobs
@@ -9,33 +11,6 @@ def getdata():
     with open("Austin_COVID-19_Complaint_Cases.json", "r") as json_data:
         userdata = json.load(json_data)
     return userdata
-
-@app.route('/helloworld', methods = ['GET'])
-def hello_world():
-    return 'hello world'
-
-### Update the complaints dataset
-@app.route('/update', methods=['GET'])
-def update():
-    uuid = request.args.get('uuid')
-    head = request.args.get('head')
-    body = request.args.get('body')
-    arms = request.args.get('arms')
-    legs = request.args.get('legs')
-    tails = request.args.get('tails')
-
-    if head is not None:
-        rd.hset(uuid, 'head', head)
-    if body is not None:
-        rd.hser(uuid, 'body', body)
-    if arms is not None:
-        rd.hset(uuid, 'arms', arms)
-    if legs is not None:
-        rd.hset(uuid, 'legs', legs)
-    if tails is not None:
-        rd.hset(uuid, 'tails', tails)
-
-    return str(uuid + 'has been updated')
 
 ### Query complaints from a range of dates
 @app.route('/query', methods = ['GET'])
@@ -69,33 +44,7 @@ def select(id):
 
     return("")
 
-### Edit a complaint by CASENUMBER
-@app.route('/edit/<CASENUMBER:id>')
-def edit(id):
-    data = getdata()
-    edit_animal = []
-
-    edit_arms = input('Edit number of arms (int) : ')
-
-    for animal in animal_list:
-        if (animal['uid'] == str(id)):
-            animal['arms'] = edit_arms
-            edit_animal.append(animal)
-
-    edit = {}
-    edit['updated stats'] = edit_animal
-
-    edited_animals = {}
-    edited_animals['animals'] = animal_list
-
-    with open('animals.json', 'w') as f:
-        json.dump(edited_animals, f, indent=2)
-
-    print("Edit was made to animal." + "\n")
-
-    return edit
-
-### Delete complaints within a range of dates
+### Delete complaints by jobid parameter
 @app.route('/delete', methods=['GET'])
 def delete():
     data = getdata()
@@ -118,21 +67,6 @@ def delete():
 
     return("Complaints have been deleted." + "\n")
 
-### Possibly compute the zipcode with the most complaints?
-@app.route('/zipcode', methods = ['GET'])
-def average():
-    data = getdata()
-    animal_list = data['animals']
-    total_legs = 0;
-    leg_average = 0;
-    number_of_animals = len(animal_list)
-    for animal in animal_list :
-        total_legs += animal['legs']
-
-    leg_average = int(total_legs/number_of_animals)
-
-    return("Average number of legs: " + str(leg_average) + "\n")
-
 ### Count the total number of complaints
 @app.route('/total', methods = ['GET'])
 def total():
@@ -140,14 +74,6 @@ def total():
     number_of_complaints = len(data)
 
     return("Total number of COVID-19 complaints: " + str(number_of_complaints) + "\n")
-
-@app.route('/jobs', methods=['POST'])
-def jobs_api():
-    try:
-        job = request.get_json(force=True)
-    except Exception as e:
-        return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
-    return json.dumps(jobs.add_job(job['start'], job['end']))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
